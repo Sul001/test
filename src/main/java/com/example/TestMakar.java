d87af510cbab5e8606e3f94dc937092fc577f02b
Sul001 <147269553+Sul001@users.noreply.github.com>
2025-03-14 23:41:46 +0300

    Revert "add foo() and bar()"
    This reverts commit d6343fc74c393ffe7a99461ace15f80f2c54a747.

--- src/main/java/com/example/TestMakar.java
+++ src/main/java/com/example/TestMakar.java
@@ -1,10 +1,4 @@
 package com.example;
 
 public class TestMakar {
-    public void foo(){
-        System.out.println("aaaa");
-    }
-    public String bar(String str){
-        return str;
-    }
 }
