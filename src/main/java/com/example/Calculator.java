99a846ebe452da80aa77609fccb3cfa80cecdaf1
Sul001 <147269553+Sul001@users.noreply.github.com>
2025-03-14 23:36:51 +0300

    add t()

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class Calculator {
-
+    public void t() {
+        TestMakar tm = new TestMakar();
+        System.out.println(tm.bar("sss"));
+        tm.foo();
+    }
 
     public int add(int a, int b) {
         return a + b;
