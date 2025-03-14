d6343fc74c393ffe7a99461ace15f80f2c54a747
Sul001 <147269553+Sul001@users.noreply.github.com>
2025-03-14 23:34:45 +0300

    add foo() and bar()

--- src/main/java/com/example/TestMakar.java
+++ src/main/java/com/example/TestMakar.java
@@ -1,4 +1,10 @@
 package com.example;
 
 public class TestMakar {
+    public void foo(){
+        System.out.println("aaaa");
+    }
+    public String bar(String str){
+        return str;
+    }
 }
